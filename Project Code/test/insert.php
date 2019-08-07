62a43459880fb08f9001d8c56502d374fb47aff3
maruf1020 <marufbillah1020@gmail.com>
2019-08-07 21:37:00 +0600

    Documentation : Add a form
    I added a form on this file.

--- Project Code/test/insert.php
+++ Project Code/test/insert.php
@@ -6,6 +6,14 @@
   </head>
   <body>
   
+  <form class="" action="insert.php" method="post">
+      <input type="text" name="fname" value="" placeholder="Insert name"> <br>
+      <input type="text" name="lname" value="" placeholder="Insert last name"> <br>
+      <input type="submit" name="save1" value="submit"> <br>
+      <!-- <button type="button" name="save1">Submit</button> -->
+
+    </form>
+  
   
     </body>
 </html>
